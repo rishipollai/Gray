631501202d42fdffa194f7a9ffdf12a12615eb80
Rishi7pillai <hrushikeshpollai777@gmail.com>
2020-08-09 18:58:55 +0530

    Updates

---
+++ precache-manifest.d91fd525b9ae2e7495cb3cf0aae86dc3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f021e39037c73325d22070d5608190c",
+    "url": "/Gray/index.html"
+  },
+  {
+    "revision": "4b5a5dc8a8b552120191",
+    "url": "/Gray/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "1dac8476c42efcfd35a8",
+    "url": "/Gray/static/css/main.2b8d48c8.chunk.css"
+  },
+  {
+    "revision": "4b5a5dc8a8b552120191",
+    "url": "/Gray/static/js/2.8a68ce9c.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/Gray/static/js/2.8a68ce9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dac8476c42efcfd35a8",
+    "url": "/Gray/static/js/main.cebbedc7.chunk.js"
+  },
+  {
+    "revision": "3851f63f8a94caa5f001",
+    "url": "/Gray/static/js/runtime-main.11c8206a.js"
+  },
+  {
+    "revision": "fab8674d0f59d1926f4576e66cd6a8f0",
+    "url": "/Gray/static/media/Busy.fab8674d.gif"
+  },
+  {
+    "revision": "b7e8ee6029d4034cb3f8b41cb63a1a7f",
+    "url": "/Gray/static/media/four.b7e8ee60.jpg"
+  },
+  {
+    "revision": "d579b83630fec99d0d17cace870d5db3",
+    "url": "/Gray/static/media/love.d579b836.gif"
+  },
+  {
+    "revision": "4bb2ddda8a9bf88e54fb475e9bd4df41",
+    "url": "/Gray/static/media/three.4bb2ddda.png"
+  }
+]);
